939d02b10907f3cbcbdde1efb30c512c77c28a7a
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-05-31 18:35:59 +0300

    My first commit!

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,8 +3,8 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         int a = 10;
-        int b = 10;
-        int c = 10;
+        int b = a;
+        int c = b;
         System.out.println(c);
     }
 }
